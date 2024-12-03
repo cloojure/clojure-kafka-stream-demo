9457f226286d5d49a09d8a6c19dc5e067ceb941e
Alan Thompson <thompson2526@gmail.com>
2024-12-02 18:41:28 -0800

    misc

--- project.clj
+++ project.clj
@@ -3,15 +3,15 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]
-                 [fundingcircle/jackdaw "0.9.11"]
-                 [org.apache.kafka/kafka-streams-test-utils "3.6.0"]
+  :dependencies [[org.clojure/clojure "1.12.0"]
+                 [fundingcircle/jackdaw "0.9.12"]
+                 [org.apache.kafka/kafka-streams-test-utils "3.9.0"]
 
-                 [org.testcontainers/testcontainers "1.19.2"]
-                 [org.testcontainers/kafka "1.19.2"]
+                 [org.testcontainers/testcontainers "1.20.4"]
+                 [org.testcontainers/kafka "1.20.4"]
 
-                 [org.slf4j/slf4j-api "2.0.9"]
-                 [org.slf4j/slf4j-simple "2.0.9"]]
+                 [org.slf4j/slf4j-api "2.0.16"]
+                 [org.slf4j/slf4j-simple "2.0.16"]]
   :main ^:skip-aot clojure-kafka-stream-demo.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
