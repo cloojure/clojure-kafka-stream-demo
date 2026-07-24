5f33730ba1b7b6873d87d0bd1f5fe4e454820423
Alan Thompson <cloojure@gmail.com>
2026-07-23 18:25:47 -0700

    misc

--- project.clj
+++ project.clj
@@ -6,23 +6,23 @@
 
   :plugins [
             [com.jakemccrary/lein-test-refresh "0.26.0"]
-            [lein-ancient "0.7.0"]
+            [lein-ancient "1.0.0"]
             ]
 
-  :dependencies [[org.clojure/clojure "1.12.4"]
+  :dependencies [[org.clojure/clojure "1.12.5"]
 
-                 [com.fasterxml.jackson.core/jackson-annotations "2.20"]
+                 [com.fasterxml.jackson.core/jackson-annotations "2.22"]
 
                  [fundingcircle/jackdaw "0.9.12"]
-                 [org.apache.kafka/kafka-streams-test-utils "4.2.0"]
+                 [org.apache.kafka/kafka-streams-test-utils "4.3.1"]
 
                  [org.testcontainers/testcontainers "2.0.5"]
                  [org.testcontainers/kafka "1.21.4"]
 
-                 [org.slf4j/slf4j-api "2.0.17"]
-                 [org.slf4j/slf4j-simple "2.0.17"]
+                 [org.slf4j/slf4j-api "2.0.18"]
+                 [org.slf4j/slf4j-simple "2.0.18"]
 
-                 [aleph "0.9.8"]
+                 [aleph "0.9.10"]
                  ]
 
   :main ^:skip-aot clojure-kafka-stream-demo.core
