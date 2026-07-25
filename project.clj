c8fa5d99e2c7c189bc04e1126e490579d8cec2b4
Alan Thompson <cloojure@gmail.com>
2026-07-25 15:50:28 -0700

    misc

--- project.clj
+++ project.clj
@@ -5,11 +5,20 @@
 
   :dependencies [[org.clojure/clojure "1.12.5"]
 
-                 [fundingcircle/jackdaw "0.9.12"]
+                 ; Kafka is capped at 3.x by jackdaw 0.9.12: it implements the
+                 ; Transformer API that Kafka 4.0 removed, so 4.x won't compile.
+                 ; 3.9.1 is the newest that works -- override jackdaw's 3.3.2 pin
+                 ; here and exclude its transitive Kafka so versions stay aligned.
+                 ; (kafka-streams-test-utils is required by jackdaw.data.producer
+                 ; for org.apache.kafka.streams.test.TestRecord.)
+                 [org.apache.kafka/kafka-clients "3.9.1"]
+                 [org.apache.kafka/kafka-streams "3.9.1"]
+                 [org.apache.kafka/kafka-streams-test-utils "3.9.1"]
 
-                 ; jackdaw.data.producer needs org.apache.kafka.streams.test.TestRecord
-                 ; from this jar; pin to jackdaw's Kafka (3.3.2) to avoid a 3.x/4.x skew.
-                 [org.apache.kafka/kafka-streams-test-utils "3.3.2"]
+                 [fundingcircle/jackdaw "0.9.12"
+                  :exclusions [org.apache.kafka/kafka-clients
+                               org.apache.kafka/kafka-streams
+                               org.apache.kafka/kafka-streams-test-utils]]
 
                  [org.testcontainers/testcontainers "1.21.4"] ; *** MUST KEEP VERSIONS IN SYNC
                  [org.testcontainers/kafka "1.21.4"]
