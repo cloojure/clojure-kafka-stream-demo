0479ec06e037dafcdaec6dbeb227fba112c82e62
Alan Thompson <cloojure@gmail.com>
2026-07-25 14:29:22 -0700

    misc

--- test/clojure_kafka_stream_demo/simple_test.clj
+++ test/clojure_kafka_stream_demo/simple_test.clj
@@ -8,11 +8,13 @@
     [jackdaw.test.fixtures :refer [topic-fixture]]
     ))
 
+(def topics {:input-topic  (topic-config (str "input-topic-" (random-uuid)))
+             :output-topic (topic-config (str "output-topic-" (random-uuid)))})
+
 (use-fixtures
-  :once
-  (topic-fixture
-    {"bootstrap.servers" (kafka-bootstrap-servers)}
-    topics))
+  :once (topic-fixture
+          {"bootstrap.servers" (kafka-bootstrap-servers)}
+          topics))
 
 (defn build-simple-kafka-stream-topology
   [topics builder]
@@ -25,33 +27,32 @@
   builder)
 
 (deftest simple-kafka-stream-test
-  (let [topics {:input-topic  (topic-config (str "input-topic-" (random-uuid)))
-                :output-topic (topic-config (str "output-topic-" (random-uuid)))}]
-    (with-open [machine (test-machine
-                          (jdt/kafka-transport
-                            {"bootstrap.servers" (kafka-bootstrap-servers)
-                             "group.id"          "simple-kafka-stream-test-machine"}
-                            topics))]
 
-      ; just a simplification for the demo, usually it will be started as part of the test system, via component or integrant
-      (with-open [kafka-stream (start-kafka-stream!
-                                 (partial build-simple-kafka-stream-topology topics))]
-        (let [write (cmd/write! :input-topic {:name "world"}
-                                {:partition 0})
-              watch (cmd/watch
-                      (fn [journal]
-                        (->> (get-in journal [:topics :output-topic])
-                          (first)))
-                      {:timeout 5000})
-              {:keys [results]} (jdt/run-test machine [write watch])
-              [_ watch-result] results]
+  (with-open [machine (test-machine
+                        (jdt/kafka-transport
+                          {"bootstrap.servers" (kafka-bootstrap-servers)
+                           "group.id"          "simple-kafka-stream-test-machine"}
+                          topics))]
 
-          (is (= {:headers   {}
-                  :key       nil
-                  :offset    0
-                  :partition 0
-                  :topic     :output-topic
-                  :value     "Hello world"}
+    ; just a simplification for the demo, usually it will be started as part of the test system, via component or integrant
+    (with-open [kafka-stream (start-kafka-stream!
+                               (partial build-simple-kafka-stream-topology topics))]
+      (let [write (cmd/write! :input-topic {:name "world"}
+                              {:partition 0})
+            watch (cmd/watch
+                    (fn [journal]
+                      (->> (get-in journal [:topics :output-topic])
+                        (first)))
+                    {:timeout 5000})
+            {:keys [results]} (jdt/run-test machine [write watch])
+            [_ watch-result] results]
 
-                 (get-in watch-result [:result :info]))))))))
+        (is (= {:headers   {}
+                :key       nil
+                :offset    0
+                :partition 0
+                :topic     :output-topic
+                :value     "Hello world"}
+
+               (get-in watch-result [:result :info])))))))
 
--- test/clojure_kafka_stream_demo/word_count_test.clj
+++ test/clojure_kafka_stream_demo/word_count_test.clj
@@ -8,7 +8,8 @@
     [jackdaw.test.commands :as cmd]
     [jackdaw.test.fixtures :refer [topic-fixture]]))
 
-
+(def topics {:input-topic (topic-config (str "input-topic-" (random-uuid)))
+              :output-topic  (topic-config (str "output-topic-" (random-uuid)))})
 
 (defn build-word-count-kafka-stream-topology
   [topics builder]
@@ -26,47 +27,46 @@
   builder)
 
 (use-fixtures
-  :once (topic-fixture
-          {"bootstrap.servers" (kafka-bootstrap-servers)}
-          topics))
+  :once
+  (topic-fixture
+    {"bootstrap.servers" (kafka-bootstrap-servers)}
+    topics))
 
 (deftest word-count-kafka-stream-test
-  (let [topics {:input-topic  (topic-config (str "input-topic-" (random-uuid)))
-                :output-topic (topic-config (str "output-topic-" (random-uuid)))}]
-    (with-open [machine (test-machine
-                          (jdt/kafka-transport
-                            {"bootstrap.servers" (kafka-bootstrap-servers)
-                             "group.id"          (str "test-machine-" (random-uuid))}
-                            topics))]
+  (with-open [machine (test-machine
+                        (jdt/kafka-transport
+                          {"bootstrap.servers" (kafka-bootstrap-servers)
+                           "group.id"          (str "test-machine-" (random-uuid))}
+                          topics))]
 
-      ; just a simplification for the demo, usually it will be started as part of the test system, via component or integrant
-      (with-open [kafka-stream (start-kafka-stream!
-                                 (partial build-word-count-kafka-stream-topology topics))]
-        (let [write-1 (cmd/write! :input-topic {:line "a b c"}
-                                  {:key       (random-uuid)
-                                   :partition 0})
-              write-2 (cmd/write! :input-topic {:line "x y z a b"}
-                                  {:key       (random-uuid)
-                                   :partition 0})
-              watch   (cmd/watch (fn [journal]
-                                   (let [output (->> (get-in journal [:topics :output-topic])
-                                                  (remove nil?))]
-                                     (when (= 8 (count output))
-                                       output))
+    ; just a simplification for the demo, usually it will be started as part of the test system, via component or integrant
+    (with-open [kafka-stream (start-kafka-stream!
+                               (partial build-word-count-kafka-stream-topology topics))]
+      (let [write-1 (cmd/write! :input-topic {:line "a b c"}
+                                {:key       (random-uuid)
+                                 :partition 0})
+            write-2 (cmd/write! :input-topic {:line "x y z a b"}
+                                {:key       (random-uuid)
+                                 :partition 0})
+            watch   (cmd/watch (fn [journal]
+                                 (let [output (->> (get-in journal [:topics :output-topic])
+                                                (remove nil?))]
+                                   (when (= 8 (count output))
+                                     output))
 
-                                   )
-                                 {:timeout 10000})
-              {:keys [results journal]} (jdt/run-test machine [write-1 write-2 watch])
-              [_ _ watch-result] results]
+                                 )
+                               {:timeout 10000})
+            {:keys [results journal]} (jdt/run-test machine [write-1 write-2 watch])
+            [_ _ watch-result] results]
 
-          (is (= [{:key "a" :value 1}
-                  {:key "b" :value 1}
-                  {:key "c" :value 1}
-                  {:key "x" :value 1}
-                  {:key "y" :value 1}
-                  {:key "z" :value 1}
-                  {:key "a" :value 2}
-                  {:key "b" :value 2}]
-                 (mapv (fn [m]
-                         (select-keys m [:key :value]))
-                       (get-in watch-result [:result :info])))))))))
+        (is (= [{:key "a" :value 1}
+                {:key "b" :value 1}
+                {:key "c" :value 1}
+                {:key "x" :value 1}
+                {:key "y" :value 1}
+                {:key "z" :value 1}
+                {:key "a" :value 2}
+                {:key "b" :value 2}]
+               (mapv (fn [m]
+                       (select-keys m [:key :value]))
+                     (get-in watch-result [:result :info]))))))))
